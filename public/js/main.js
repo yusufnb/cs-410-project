0cc3c44ff91f6160322de63552742be86e591aea
Yusuf Bhabhrawala <yusufnb@gmail.com>
2017-12-20 09:56:08 -0800

    Working events

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,23 @@
 $(function(){
+
+	function reloadMarkers() {
+		console.log(dateSlider.val());
+		console.log(map.getBounds().getNorthEast().toJSON());
+		console.log(map.getBounds().getSouthWest().toJSON());
+	}
+
 	var map = new GMaps({
 	  div: '#map',
 	  lat: -12.043333,
-	  lng: -77.028333
+	  lng: -77.028333,
+	  zoom: 2,
+	  dragend: reloadMarkers,
+	  zoom_changed: reloadMarkers
 	});
+
+	var dateSlider = $("#date-slider").slider({});
+	dateSlider.on('slideStop', reloadMarkers);
+
+	window.map = map;
+	window.dateSlider = dateSlider;
 })
